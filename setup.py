2505aff921f9be98d004ed565e016b768c0d6e1c
Daniel shi <xiaoliang.shi@mail.utoronto.ca>
2018-10-04 00:13:39 -0400

    add cat gif

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='bunny',
-    version='0.3',
-    description='A cute bunny to stare at until training converges',
-    url='http://github.com/bheinzerling/bunny',
-    author='Benjamin Heinzerling',
-    author_email='benjamin.heinzerling@h-its.org',
+    version='0.4',
+    description='Initiated from the bunny project, a cute emocon to stare at until training converges',
+    url='http://github.com/demoonism/bunny',
+    author='Benjamin Heinzerling, Daniel Shi',
+    author_email='danielxlshi@gmail.com',
     license='MIT',
     packages=['bunny'],
     zip_safe=False)
